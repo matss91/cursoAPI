a09f60f523d0c17c16ffd0ca2c90a2ea7bbffe7b
matias cardillo <matcardillo91@gmail.com>
2025-03-22 18:27:56 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express=require("express")
+const connectDB = require('./config/db');
+const router = require('./routes/index');
+const createHttpError = require('http-errors');
+const cors = require('cors');
+
+
+const app = express();
+const allowedOrigins = ['http://localhost:5173'];
+
+const options= {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api',router);
+
+app.use(function(req, res, next){
+    const err = createHttpError(404, 'Not Found');
+    next(err);
+  });
+
+app.use(function(err , req, res , next ) {
+  
+    return res.status(err.status || 500).json({
+      ok : false,
+      msg : err.message
+      });
+  });
+
+/* app.listen(process.env.PORT, ()=> {
+console.log(`Server running in https://localhost:${process.env.PORT}`);
+}); */
+
+module.exports=app
